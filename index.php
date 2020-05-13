b8fb075bd1862bd7861676e076bd504139c5cbc3
Ayrton Ottnoi <1726632@johnabbottcollege.net>
2020-05-12 20:52:57 -0400

    A3 starter

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Quarantine Supply</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <img src="photos/banner.png">
+        </header>
+        <nav>
+            <button>Products</button>
+            <button>About</button>
+            <button>Profile</button>
+            <button>Cart</button>
+            <button>Carti<a href="photos/carti.png"></a></button> 
+        </nav>
+    </body>
+</html>
