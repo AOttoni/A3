ec83e1c664cae6c51f9c2ad2f0f6a5dc791df3f8
Eric ZL <1776688@johnabbottcollege.net>
2020-05-24 17:43:00 -0400

    Updated the address/payment controllers

--- controllers/get_address.controller.php
+++ controllers/get_address.controller.php
@@ -1,8 +1,8 @@
 <?php
 require_once('database.controller.php');
 $id = $_SESSION['id'];
-$statement = $connection->prepare("SELECT `address_id`, `customer_id` FROM `customer_address` WHERE `customer_id` = ?");
-$statement->bind_param('i', $id);   
+$statement = $connection->prepare("SELECT address_id, customer_id FROM customer_address WHERE customer_id = ?");
+$statement->bind_param('i', $id);
 $statement->execute();
 $statement->bind_result($address_id, $customer_id);
 $customer_addresses = [];
@@ -12,13 +12,17 @@ for ($i = 0; $statement->fetch(); $i++) {
 $statement->close();
 $addresses = []; 
 $address;
+
 for ($i = 0; $i < count($customer_addresses); $i++){
+    if(empty($customer_addresses)){
+        break;
+    }
     $address_id = $customer_addresses[$i]['address_id'];
-    $statement = $connection->prepare("SELECT `id`, `address` FROM `address` WHERE `id` = ?");
+    $statement = $connection->prepare("SELECT id, address FROM address WHERE id = ?");
     $statement->bind_param('i', $address_id);
     $statement->execute();
     $statement->bind_result($id, $address);
     $statement->fetch();
     $addresses[$i] = ['address_id' => $id, 'address' => $address];
-}  
-$statement->close();
+    $statement->close();
+}
--- controllers/get_payment_methods.controller.php
+++ controllers/get_payment_methods.controller.php
@@ -1,8 +1,8 @@
 <?php
 require_once('database.controller.php');
 $id = $_SESSION['id'];
-$statement = $connection->prepare("SELECT `method_id`, `customer_id` FROM `customer_payment_method` WHERE `customer_id` = ?");
-$statement->bind_param('i', $id);   
+$statement = $connection->prepare("SELECT method_id, customer_id FROM customer_payment_method WHERE customer_id = ?");
+$statement->bind_param('i', $id);
 $statement->execute();
 $statement->bind_result($method_id, $customer_id);
 $customer_methods = [];
@@ -13,13 +13,16 @@ $statement->close();
 $card_numbers = []; 
 $card_number;
 for ($i = 0; $i < count($customer_methods); $i++){
+    if(empty($customer_methods)){
+        break;
+    }
     $method_id = $customer_methods[$i]['method_id'];
-    $statement = $connection->prepare("SELECT `id`, `card_number` FROM `payment_method` WHERE `id` = ?");
+    $statement = $connection->prepare("SELECT id, card_number FROM payment_method WHERE id = ?");
     $statement->bind_param('i', $method_id);
     $statement->execute();
     $statement->bind_result($id, $card_number);
     $statement->fetch();
     $card_numbers[$i] = ['method_id' => $id, 'card_number' => $card_number];
-}  
-$statement->close();
+    $statement->close();
+}
 
